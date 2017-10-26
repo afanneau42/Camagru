7474647134af601445baec4c78a14e9f9373dcbe
afanneau <afanneau@e1r10p12.42.fr>
2017-10-26 16:18:21 +0200

    no more fatal error when databse already create on function database.php

---
+++ functions/f_insert.php
@@ -0,0 +1,14 @@
+<?PHP
+    
+    function insert_user($dbh, $id, $username, $passwordd, $email, $rank)
+    {
+        $sql = $dbh->prepare("
+        INSERT INTO user
+        SELECT * FROM (SELECT ?, ?, ?, ?, ?, ?) AS tmp
+        WHERE NOT EXISTS (
+            SELECT username FROM user WHERE username = ?
+        ) LIMIT 1;
+        ");
+        $sql->execute(array($id, $username, $password, $email, $rank, 0,$username));
+    }
+?>
